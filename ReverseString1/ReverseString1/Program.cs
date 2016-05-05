24f2116e3648d1b2775f67699c4bbb44520bb3ef
Iris Caffin <iriscaffin@gmail.com>
2016-05-04 22:15:49 -0700

    Reverse a string using while loop

---
+++ ReverseString1/ReverseString1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, reversestring = "";
+
+            int Length;
+
+            Console.Write("Enter a string: ");
+
+            Str = Console.ReadLine();
+
+            Length = Str.Length - 1;
+
+            while (Length >= 0)
+            {
+                reversestring = reversestring + Str[Length];
+                Length--;
+            }
+
+            Console.WriteLine("Reverse string is: {0}", reversestring);
+            Console.ReadLine();
+        }
+    }
+}
